32f8755d51aba647e6564e1d3220e667b669d480
Jose Fabian Benavides Moreno <jose.benavides@globant.com>
2020-03-23 20:46:59 -0500

    CleanWebpackPlugin added to config, deleted dist folder to create new bundle.

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   output: {
-    filename: 'bundle.js'
+    filename: 'bundle.[hash].js'
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
-    })
+    }),
+    new CleanWebpackPlugin()
   ],
   module: {
     rules: [

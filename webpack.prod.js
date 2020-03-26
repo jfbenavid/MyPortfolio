4cbf4fb9efa1bb5835ae40cddb2fd94d2e519c18
Jose Fabian Benavides Moreno <jose.benavides@globant.com>
2020-03-26 10:12:47 -0500

    added new plugin to create a CNAME file to pointing to my domain.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,17 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CnameWebpackPlugin = require('cname-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
   output: {
     filename: 'bundle.[hash].js'
   },
-  plugins: [new CleanWebpackPlugin()]
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CnameWebpackPlugin({
+      domain: 'jfbenavid.com'
+    })
+  ]
 })
